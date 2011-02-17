c02fdad37fc6f0f5c74bf9d7d3e16df4d05b3ced
stussak <stussak@4beaa02c-d2ff-4d45-9dad-6a4a666722bb>
2011-02-17 18:49:26 +0000

    prototype version of gallery management
    git-svn-id: svn+ssh://hausach.mfo.de/localhome/imaginar/svn/imaginary/jsurfer/branches/JFXSurfer@287 4beaa02c-d2ff-4d45-9dad-6a4a666722bb

---
+++ src/de/mfo/jsurfer/gui/Gallery.java
@@ -0,0 +1,193 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package de.mfo.jsurfer.gui;
+
+import java.util.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import java.net.*;
+import java.io.*;
+import java.nio.*;
+import com.sun.pdfview.*;
+
+/**
+ *
+ * @author stussak
+ */
+public class Gallery {
+
+    ResourceBundle rb;
+    Locale locale;
+
+    int number;
+    String key;
+    String name;
+    BufferedImage icon;
+    BufferedImage description;
+    GalleryItem[] gallery_items;
+
+    int gallery_description_width;
+    int gallery_description_height;
+
+    int gallery_entry_description_width;
+    int gallery_entry_description_height;
+    
+    public Gallery( int number )
+            throws IOException
+    {
+        locale = Locale.getDefault();
+        rb = ResourceBundle.getBundle( "de/mfo/jsurfer/gui/gallery/Gallery", locale );
+        
+        Enumeration< String > keys = rb.getKeys();
+        for(; keys.hasMoreElements(); ) 
+            System.out.println( keys.nextElement() );
+         
+
+        this.gallery_entry_description_width = 600;
+        this.gallery_entry_description_height = 430;
+
+        this.gallery_entry_description_width = 380;
+        this.gallery_entry_description_height = 2*380;
+        
+        this.number = number;
+        this.key = rb.getString( "gallery_" + number + "_key" );
+        this.name = rb.getString( key );
+        this.icon = loadImage( getResource( "gallery/" + key + "_icon.png" ) );
+        this.description = renderPDF( getResourceFromLocalizedName( "gallery/" + key + "_description", ".pdf" ), gallery_entry_description_width, gallery_entry_description_height );
+        
+        String[] content_keys = getContentKeys();
+        LinkedList< GalleryItem > l = new LinkedList< GalleryItem >();
+        for( int i = 0; i < content_keys.length; i++ )
+            l.add( new GalleryItem( content_keys[ i ] ) );
+        this.gallery_items = l.toArray( new GalleryItem[ 0 ] );
+    }
+    
+    public int getNumber() { return number; }
+    public String getKey() { return key; }
+    public String getName() { return name; }
+    public BufferedImage getIcon() { return icon; }
+    public BufferedImage getDescription() { return description; }           
+    
+    public GalleryItem[] getEntries()
+    {
+        return this.gallery_items;
+    }
+
+    String[] getContentKeys()
+    {
+        String content_string = rb.getString( "gallery_" + number + "_content" );
+        String[] content_keys = content_string.split( "," );
+        for( int i = 0; i < content_keys.length; i++ )
+            content_keys[ i ] = content_keys[ i ].trim();
+        return content_keys;
+    }
+
+    URL getResource( String res )
+    {
+        URL url = getClass().getResource( res );
+        if( url == null )
+            System.err.println( "resource \"" + res + "\" not found" );
+        return url;
+    }
+
+    URL getResourceFromLocalizedName( String name, String extension )
+    {
+        // prepare localized urls
+        String url_language_country = name + "_" + locale.getLanguage() + "_" + locale.getCountry() + extension;
+        String url_language = name + "_" + locale.getLanguage() + extension;
+
+        // try to retrieve fully localized url
+        URL url = getClass().getResource( url_language_country );
+        if( url == null )
+            // fallback to language-only localization
+            url = getClass().getResource( url_language );
+
+        if( url == null )
+            System.err.println( "resource \"" + url_language_country + "\" and fallback \"" + url_language + "\" not found" );
+        return url;
+    }
+
+    BufferedImage renderPDF( URL url, int width, int height )
+    {
+        BufferedImage bImg = new BufferedImage( width, height, BufferedImage.TYPE_INT_RGB );
+        try
+        {
+            URLConnection connection = url.openConnection();
+            // Since you get a URLConnection, use it to get the InputStream
+            InputStream is = connection.getInputStream();
+            // Now that the InputStream is open, get the content length
+            int contentLength = connection.getContentLength();
+
+            // create temporary buffer
+            ByteArrayOutputStream tmpOut = new ByteArrayOutputStream( contentLength == -1 ? 16384 : contentLength );
+
+            // fill temporary buffer
+            byte[] buf = new byte[512];
+            int len;
+            while( ( len = is.read(buf) ) != -1 )
+                tmpOut.write(buf, 0, len);
+        
+            // close buffers
+            is.close();
+            tmpOut.close();
+
+            // create ByteBuffer, which we need for the PDFRenderer
+            ByteBuffer byte_buf = ByteBuffer.wrap( tmpOut.toByteArray() );
+
+            // render PDF into image
+            PDFFile pdfFile = new PDFFile( byte_buf );
+            PDFPage pdfPage = pdfFile.getPage( 0 );
+            java.awt.geom.Rectangle2D r2d = pdfPage.getBBox();
+            Image img = pdfPage.getImage( width, height, r2d, null, true, true );
+            bImg.getGraphics().drawImage( img, 0, 0, null );
+        }
+        catch( Exception e )
+        {
+            System.err.println( e );
+        }
+        return bImg;
+    }
+
+    BufferedImage loadImage( URL img_url )
+    {
+        BufferedImage img = new BufferedImage( 1, 1, BufferedImage.TYPE_INT_RGB );
+        try
+        {
+            img = ImageIO.read( img_url );
+        }
+        catch( Exception e )
+        {
+            System.err.println( "could not load " + img_url );
+        }
+        return img;
+    }
+
+    public class GalleryItem
+    {
+        private String key;
+        private String name;
+        private BufferedImage icon;
+        private BufferedImage description;
+        private URL jsurf_file_url;
+
+        GalleryItem( String key )
+                throws IOException
+        {
+            this.key = key;
+            this.name = rb.getString( key );
+            this.icon = loadImage( getResource( "gallery/" + key + "_description.pdf" ) );
+            this.description = renderPDF( getResourceFromLocalizedName( "gallery/" + key + "_description", ".pdf" ), gallery_entry_description_width, gallery_entry_description_height );
+            this.jsurf_file_url = getResource( "gallery/" + key + ".jsurf" );
+        }
+
+        public String getKey() { return key; }
+        public String getName() { return name; }
+        public BufferedImage getIcon() { return icon; }
+        public BufferedImage getDescription() { return description; }
+        public URL getJSurfURL() { return jsurf_file_url; }
+    }
+}
