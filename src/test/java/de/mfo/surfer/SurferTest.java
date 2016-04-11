c2e296db7a9457ce0ecfd0ff077f0f07d562d30d
Christian Stussak <stussak@mfo.de>
2016-04-11 20:31:54 +0200

    Add JUnit testing framework

---
+++ src/test/java/de/mfo/surfer/SurferTest.java
@@ -0,0 +1,13 @@
+package de.mfo.surfer;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class SurferTest
+{
+    @Test
+    public void dummyTest()
+    {
+        Assert.assertTrue( "this must always be true", true );
+    }
+}
