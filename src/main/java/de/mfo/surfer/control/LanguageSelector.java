30f5e826c922845dfab8fa6abed4913c0cdad897
Christian Stussak <stussak@mfo.de>
2017-10-11 10:19:54 +0200

    Determine language endonyms via locale.getDisplayName(locale)
    This makes the "language=..." properties obsolete.

--- src/main/java/de/mfo/surfer/control/LanguageSelector.java
+++ src/main/java/de/mfo/surfer/control/LanguageSelector.java
@@ -37,12 +37,25 @@ public class LanguageSelector extends ChoiceBox< Locale >
                 @Override
                 public String toString( Locale locale )
                 {
-                    return L.l( locale, "language" ) + " (" + locale + ")";
+                    String displayName = locale.getDisplayName( locale );
+
+                    // turn first letter into uppercase
+                    displayName = java.util.stream.IntStream.concat(
+                        java.util.stream.IntStream.of(displayName.codePointAt(0)).map(Character::toUpperCase),
+                        displayName.codePoints().skip(1)
+                        ).collect(
+                            StringBuilder::new,
+                            StringBuilder::appendCodePoint,
+                            StringBuilder::append
+                        ).toString();
+
+                    return //"(" + locale + ") " +
+                        displayName;
                 }
             }
         );
 
-        ObservableList items = getItems();
+        ObservableList<Locale> items = getItems();
         L.getAvailableLocales().forEach( l -> items.add( l ) );
         valueProperty().bindBidirectional( L.localeProperty() );
     }
